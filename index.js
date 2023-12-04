03bb668f5bffd93ad7392c70b06e189af82c2fe3
TylerCharlton <TyCharlton1993@gmail.com>
2023-12-04 10:48:03 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log('Wrapped ${gifts[i]} and added a bow!');
+        debugger;
+    }
+
+    return gifts;
+}
+
+
+wrapGifts(gifts);
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+
+
+
+
+function writeCards(names, eventName) {
+    let thankYouCards = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouCards.push(message);
+    }
+  
+    return thankYouCards;
+  }
+
+
+
+
